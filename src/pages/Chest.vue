e7e67a89ea6461a3d29a8fa9756f0be73d69cb5d
BrynjarGeir <brynjargeir1@gmail.com>
2021-11-16 14:55:05 +0000

    laga hitt og þetta, bæta við squat goal

--- src/pages/Chest.vue
+++ src/pages/Chest.vue
@@ -6,6 +6,8 @@
     <div class='row'><div class='col'><q-input type="number" placeholder="Weight of exercise" v-model="w12"/></div><div class='col'><q-input type="number" placeholder="Number of iterations" v-model="n12"/></div></div>
     <div class='row'><div class='col'><q-input type="number" placeholder="Weight of exercise" v-model="w13"/></div><div class='col'><q-input type="number" placeholder="Number of iterations" v-model="n13"/></div></div> 
     <div class='row'><div class='col'><q-input type="number" placeholder="Weight of exercise" v-model="w14"/></div><div class='col'><q-input type="number" placeholder="Number of iterations" v-model="n14"/></div></div>
+    <div class='row'><div class='col'><q-input type="number" placeholder="Weight of exercise" v-model="w15"/></div><div class='col'><q-input type="number" placeholder="Number of iterations" v-model="n15"/></div></div>
+
     <q-btn @click="ex1"> Log exercise </q-btn> 
     
     <h6 class="q-mt-none"> Inclined Chest Press </h6>
@@ -69,7 +71,7 @@ export default defineComponent({
 
   data() {
     return {
-        w11: null, w12: null, w13: null, w14: null,
+        w11: null, w12: null, w13: null, w14: null, w15: null,
         w21: null, w22: null, w23: null,
         w31: null, w32: null, w33: null,
         w51: null, w52: null, w53: null,
@@ -77,7 +79,7 @@ export default defineComponent({
         w71: null, w72: null, w73: null,
         w81: null, w82: null, w83: null,
         w91: null, w92: null, w93: null,
-        n11: null, n12: null, n13: null, n14: null,
+        n11: null, n12: null, n13: null, n14: null, n15: null,
         n21: null, n22: null, n23: null,
         n31: null, n32: null, n33: null,
         n41: null, n42: null, n43: null,
@@ -91,20 +93,20 @@ export default defineComponent({
 
   methods: {
     async ex1() {
-      await setDoc(doc(db, 'chest', 'ChestPress' + Date.now()), {
+      await setDoc(doc(db, 'chest', 'Chest-Press' + Date.now()), {
           name: 'Chest Press',
-          weight: [parseInt(this.w11), parseInt(this.w12), parseInt(this.w13), parseInt(this.w14)],
-          iterattions:  [parseInt(this.n11), parseInt(this.n12), parseInt(this.n13), parseInt(this.n14)],
+          weight: [parseInt(this.w11), parseInt(this.w12), parseInt(this.w13), parseInt(this.w14), parseInt(this.w15)],
+          iterations:  [parseInt(this.n11), parseInt(this.n12), parseInt(this.n13), parseInt(this.n14), parseInt(this.n15)],
           date: Date.now()
       }).then(
-          this.w11 = null, this.w12 = null, this.w13 = null, this.w14 = null,
-        this.n11 = null, this.n12 = null, this.n13 = null, this.n14 = null
+          this.w11 = null, this.w12 = null, this.w13 = null, this.w14 = null, this.w15 = null,
+        this.n11 = null, this.n12 = null, this.n13 = null, this.n14 = null, this.n15 = null
       )
 
     },
     async ex2() {
-      await setDoc(doc(db, 'chest', 'Pull-up' + Date.now()), {
-          name: 'Pull up',
+      await setDoc(doc(db, 'chest', 'Inclined-Chest-Press' + Date.now()), {
+          name: 'Inclined Chest Press',
           weight: [parseInt(this.w21), parseInt(this.w22), parseInt(this.w23)],
           iterations: [parseInte(this.n21), parseInt(this.n22), parseInt(this.n23)],
           date: Date.now()
@@ -115,8 +117,8 @@ export default defineComponent({
 
     },
     async ex3() {
-      await setDoc(doc(db, 'chest', 'Single-arm-row' + Date.now()), {
-          name: 'Single-arm row',
+      await setDoc(doc(db, 'chest', 'Cable-Fly' + Date.now()), {
+          name: 'Cable Fly',
           weight:  [parseInt(this.w21), parseInt(this.w22), parseInt(this.w23)],
           iterations: [parseInte(this.n21), parseInt(this.n22), parseInt(this.n23)],
           date: Date.now()
@@ -127,8 +129,8 @@ export default defineComponent({
 
     },
     async ex4() {
-      await setDoc(doc(db, 'chest', 'Inverted-row' + Date.now()), {
-          name: 'Inverted row',
+      await setDoc(doc(db, 'chest', 'Press-Up' + Date.now()), {
+          name: 'Press Up',
           iterations:  [parseInt(this.n41), parseInt(this.n42), parseInt(this.n43)],
           date: Date.now()
       }).then(
@@ -136,8 +138,8 @@ export default defineComponent({
       )
     },
     async ex5() {    
-      await setDoc(doc(db, 'chest', 'RenegadeRow' + Date.now()), {
-          name: 'Renegade row',
+      await setDoc(doc(db, 'chest', 'Narrow-Press-Up' + Date.now()), {
+          name: 'Narrow Press Up',
           weight: [parseInt(this.w51), parseInt(this.w52), parseInt(this.w53)],
           iterattions: [parseInt(this.n51), parseInt(this.w52), parseInt(this.w53)],
           date: Date.now()
@@ -148,8 +150,8 @@ export default defineComponent({
 
     },
     async ex6() {
-      setDoc(doc(db, 'chest', 'LatPulldown' + Date.now()), {
-          name: 'Lat Pulldown',
+      setDoc(doc(db, 'chest', 'Dumbbell-Chest-Press' + Date.now()), {
+          name: 'Dumbbell Chest Press',
           weight: [parseInt(this.w61), parseInt(this.w62), parseInt(this.w63)],
           iterations: [parseInt(this.n61), parseInt(this.w62), parseInt(this.w63)],
           date: Date.now()
@@ -160,17 +162,38 @@ export default defineComponent({
 
     },
     async ex7() {
-      await setDoc(doc(db, 'chest', 'Seated-Cable-Row' + Date.now()), {
-          name: 'Seated Cable Row',
+      await setDoc(doc(db, 'chest', 'Dumbbell-Chest-Fly' + Date.now()), {
+          name: 'Dumbbell Chest Fly',
           weight: [parseInt(this.w71), parseInt(this.w72), parseInt(this.w73)],
-          iterations: [parseInt(this.n61), parseInt(this.w62), parseInt(this.w63)],
+          iterations: [parseInt(this.n71), parseInt(this.n72), parseInt(this.n73)],
           date: Date.now()
       }).then(
             this.w71 = null, this.w72 = null, this.w73 = null,
             this.n71 = null, this.n72 = null, this.n73 = null          
       )
-
-    }      
+    },
+    async ex8() {
+      await setDoc(doc(db, 'chest', 'Inclined-Dumbbell-Chest-Fly' + Date.now()), {
+          name: 'Inclined Dumbbell Chest Fly',
+          weight: [parseInt(this.w81), parseInt(this.w82), parseInt(this.w83)],
+          iterations: [parseInt(this.n81), parseInt(this.w82), parseInt(this.w83)],
+          date: Date.now()
+      }).then(
+            this.w81 = null, this.w82 = null, this.w83 = null,
+            this.n81 = null, this.n82 = null, this.n83 = null          
+      )
+    },
+    async ex9() {
+      await setDoc(doc(db, 'chest', 'Machine-Fly' + Date.now()), {
+          name: 'Machine Fly',
+          weight: [parseInt(this.w91), parseInt(this.w92), parseInt(this.w93)],
+          iterations: [parseInt(this.n91), parseInt(this.w92), parseInt(this.w93)],
+          date: Date.now()
+      }).then(
+            this.w91 = null, this.w92 = null, this.w93 = null,
+            this.n91 = null, this.n92 = null, this.n93 = null          
+      )
+    }               
   }
 })
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,10 +2,11 @@
   <q-page class="flex flex-center">
     <div class="q-pa-md bg-grey-10 text-white">
     <div class="q-gutter-sm">
-      <q-checkbox dark disable v-model="teal" label="Weight at under 82kg" color="teal" /><br>
-      <q-checkbox dark disable v-model="orange" label="Fat percentage at under 10%" color="orange" /><br>
-      <q-checkbox dark disable v-model="red" label="Benching at/over 100kg" color="red" /><br>
+      <q-checkbox dark disable v-model="teal" label="Weight at/under 82kg"  color="teal" /><br>
+      <q-checkbox dark disable v-model="orange" label="Fat percentage at under 15%" color="orange" /><br>
+      <q-checkbox dark disable v-model="red" label="Benching at/over 90kg" color="red" /><br>
       <q-checkbox dark disable v-model="cyan" label="Deadlift at/over 150kg" color="cyan" /><br>
+      <q-checkbox dark disable v-model="magenta" label="Squat at/over 100kg" color="magenta" /><br>
     </div>
   </div>
   </q-page>
@@ -21,7 +22,7 @@ export default defineComponent({
   name: 'PageIndex',
   data() {
     return {
-      teal: null, orange: null, red: null, cyan: null
+      teal: null, orange: null, red: null, cyan: null, magenta: null
     }
   },
   mounted() {
@@ -29,7 +30,7 @@ export default defineComponent({
   },
   methods: {
     async getCheckboxData() {
-      let currWeight, currFatPer, currBenchWeight, currDeadWeight
+      let currWeight, currFatPer, currBenchWeight, currDeadWeight, currSquatWeight
         const qWeight = query(collection(db, 'weight'), orderBy('date', 'desc'), limit(1))
         const qWeightSnapshot = await getDocs(qWeight)
         qWeightSnapshot.forEach((doc) => {
@@ -40,19 +41,25 @@ export default defineComponent({
         const qFatSnapshot = await getDocs(qFat)
         qFatSnapshot.forEach((doc) => {
           currFatPer = doc.data().percentage
-          if (currFatPer <= 10) { this.orange = ref(true) } else { this.orange = ref(false) }
+          if (currFatPer <= 15) { this.orange = ref(true) } else { this.orange = ref(false) }
         })
         const qBench = query(collection(db, 'chest'), where('name', '==', 'Chest Press'), orderBy('date', 'desc'), limit(1))
         const qBenchSnapshot = await getDocs(qBench)
         qBenchSnapshot.forEach((doc) => {
           currBenchWeight = Math.max(...doc.data().weight)
-          if (currBenchWeight >= 100) { this.red = ref(true) } else { this.red = ref(false) }
+          if (currBenchWeight >= 90) { this.red = ref(true) } else { this.red = ref(false) }
         })
         const qDeadlift = query(collection(db, 'back'), where('name', '==', 'Deadlift'), orderBy('date', 'desc'), limit(1))
         const qDeadliftSnapshot = await getDocs(qDeadlift)
         qDeadliftSnapshot.forEach((doc) => {
           currDeadWeight = Math.max(...doc.data().weight)
           if (currDeadWeight >= 150) { this.cyan = ref(true) } else { this.cyan = ref(false) }
+        })
+        const qSquat = query(collection(db, 'legs'), where('name', '==', 'Squat'), orderBy('date', 'desc'), limit(1))
+        const qSquatSnapshot = await getDocs(qSquat)
+        qSquatSnapshot.forEach((doc) => {
+          currSquatWeight = Math.max(...doc.data().weight)
+          if (currSquatWeight >= 100) { this.magenta = ref(true) } else { this.magenta = ref(false) }
         })
       }
     }
